726c28d43f50b086247583bda48d488b7394097b
Jonas Almeida <jalmeida@mathbiol.org>
2019-05-21 20:48:35 -0400

    redirecting from local service to heroku service

--- firebrowse.js
+++ firebrowse.js
@@ -8,7 +8,7 @@ firebrowse.created_at=Date()
 
 firebrowse.get=async function(url){
     url = url||'http://firebrowse.org/api/v1/Analyses/Mutation/MAF?format=json&cohort=BLCA&tool=MutSig2CV&gene=PSCA&page=1&page_size=250&sort_by=tcga_participant_barcode'
-    return (await fetch('http://localhost:3000/?'+url)).json()
+    return (await fetch('https://firebrowse.herokuapp.com/?'+url)).json()
 }
 
 
